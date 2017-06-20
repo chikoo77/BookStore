74cea1d6f94ccab7f2138350a8a32e0381b5ae3c
Frank <frank.lm7787@gmail.com>
2017-06-20 02:02:35 -0600

    Google on

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616191528) do
+ActiveRecord::Schema.define(version: 20170620075140) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20170616191528) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
